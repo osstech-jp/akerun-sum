782532123cf82fc40e48de9c8961b75626d02d9f
mypaceshun <shun@isasaka.jp>
2017-02-28 06:02:48 +0900

    dir reflesh
    remove files

--- test/run.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-python3 ../akerun-sum.py -i input-utf8.csv -o output.csv -d 201610
-python3 ../akerun-sum.py -i input-sjis.csv -o output.csv -d 201610
-python3 ../akerun-sum.py -i input-euc.csv -o output.csv -d 201610
-
