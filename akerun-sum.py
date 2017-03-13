bda9397cc166ecf1e877db0843ba2425b0922be5
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2017-03-13 13:59:46 +0900

    Whitespace changes

--- akerun-sum.py
+++ akerun-sum.py
@@ -27,7 +27,7 @@ def option_parser():
     if len(arguments) == 9:
         index_number = [1, 3, 5, 7]
 
-# default
+    # default
     format_num = 0
     for index in index_number:
         option = arguments[index]
@@ -53,7 +53,6 @@ def option_parser():
 
 
 def input_data(filename):
-
     lookup = ('utf_8', 'euc_jp', 'euc_jis_2004', 'euc_jisx0213',
               'shift_jis', 'shift_jis_2004', 'shift_jisx0213',
               'iso2022jp', 'iso2022_jp_1', 'iso2022_jp_2', 'iso2022_jp_3',
@@ -84,7 +83,6 @@ def input_data(filename):
 
 
 def data_shaping(data_list, period):
-
     # 2:00 -> 23:00
     date_format_list = ['%Y/%m/%d %H:%M', '%Y-%m-%d %H:%M:%S']
     day_start = datetime.datetime.strptime(DAYSTART, '%H%M')
